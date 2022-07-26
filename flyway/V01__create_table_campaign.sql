3487ac523a72c8c5d0c9d36f3dca6e230c3f9793
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2022-07-26 18:58:49 +0100

    Remove unused flies

--- flyway/V01__create_table_campaign.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE campaign
-(
-    id bigint auto_increment primary key,
-    name varchar(255) null
-);
-
--- flyway/V02__insert_campaign_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO subscription.campaign (name) VALUES ('Marketing');
-INSERT INTO subscription.campaign (name) VALUES ('Travel');
-INSERT INTO subscription.campaign (name) VALUES ('Concerts');
