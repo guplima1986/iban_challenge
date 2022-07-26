dc0a25ace4e9c9ab0067b45994a9a25045b410e2
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2022-07-26 18:58:17 +0100

    Fix rabbimq erros when call a container Remove unused flies Remove Flyway Create a Mock inserts campaign in SubscriptionApplication

--- subscription/src/main/java/com/iban/subscription/SubscriptionApplication.java
+++ subscription/src/main/java/com/iban/subscription/SubscriptionApplication.java
@@ -1,6 +1,10 @@
 package com.iban.subscription;
 
+import com.iban.core.model.Campaign;
+import com.iban.core.repository.CampaingRepository;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -10,10 +14,21 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan({ "com.iban.core.model"})
 @EnableJpaRepositories({"com.iban.core.repository"})
 @EnableRabbit
-public class SubscriptionApplication {
+public class SubscriptionApplication implements CommandLineRunner {
+
+	@Autowired
+	private CampaingRepository campaingRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SubscriptionApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		campaingRepository.save(new Campaign(null, "Health"));
+		campaingRepository.save(new Campaign(null, "Sports"));
+		campaingRepository.save(new Campaign(null, "Travel"));
+
+	}
 }
